cc4294a35c73f40d970fb604fe9e8bc0a37e6ec5
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-09-12 16:34:31 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+import FaceAntiSpoofing from "./components/FaceAntiSpoofing";
+
+function App() {
+  return (
+    <div className="App">
+      <FaceAntiSpoofing />
+    </div>
+  );
+}
+
+export default App;
